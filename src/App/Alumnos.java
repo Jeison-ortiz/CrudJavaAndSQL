b9352990d0730d7d973121914d8d75551087d472
Jeison Ortiz <ortizjeison4@gmail.com>
2023-02-14 23:45:13 -0500

    feat(alumnos): Se agrega metodo para limpiar formulario y se muestran los alumnos agregados

--- src/App/Alumnos.java
+++ src/App/Alumnos.java
@@ -8,6 +8,10 @@ import javax.swing.JOptionPane;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import javax.swing.ButtonGroup;
+import javax.swing.table.DefaultTableModel;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 
 /**
  *
@@ -23,6 +27,7 @@ public class Alumnos extends javax.swing.JFrame {
         btnGr = new ButtonGroup();
         btnGr.add(rbMasculino);
         btnGr.add(rbFemenino);
+        cargarTabla();
     }
     
     
@@ -314,6 +319,7 @@ public class Alumnos extends javax.swing.JFrame {
             ps.executeUpdate();
             JOptionPane.showMessageDialog(null,"Registro guardado");
             limpiar();
+            cargarTabla();
             
         } catch (Exception e) {
              JOptionPane.showMessageDialog(null,e.toString());
@@ -338,6 +344,36 @@ public class Alumnos extends javax.swing.JFrame {
     txtEmail.setText("");
     btnGr.clearSelection();
     }
+    
+    private void cargarTabla(){
+        DefaultTableModel modeloTabla = (DefaultTableModel) tblAlumnos.getModel();
+        modeloTabla.setRowCount(0);
+        
+        PreparedStatement ps;
+        ResultSet rs;
+        ResultSetMetaData rsmd;
+        int columnas;
+        
+        try{
+            Conexion conexion = new Conexion();
+            Connection con = conexion.getConexion();
+            ps = con.prepareStatement("SELECT id, matricula,nombre,sexo,email from alumnos");
+            rs = ps.executeQuery();
+            rsmd = rs.getMetaData();
+            columnas = rsmd.getColumnCount();
+            
+            while(rs.next()){
+                Object[] fila = new Object[columnas];
+                for(int indice = 0; indice<columnas; indice ++){
+                    fila[indice]=rs.getObject(indice +1);
+                }
+                modeloTabla.addRow(fila);
+            }
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, e.toString());
+        }
+    }
+    
     
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
